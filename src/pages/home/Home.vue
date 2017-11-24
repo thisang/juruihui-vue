4f9903559ad503a1e4504231462abf54d9397a9b
thisang <thisang@foxmail.com>
2017-11-25 00:42:56 +0800

    首页

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <header>聚蕊烩</header>
+    <router-view></router-view>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+<script>
+import { XHeader } from 'vux'
+export default {
+  components: {
+    XHeader
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .home{
+    header{
+      width: 100%;
+      position: fixed;
+      top: 0;
+      background-color: #58b256;
+    }
+    footer{
+      width: 100%;
+      position: fixed;
+      bottom: 0;
+      border-top: 1px solid red;
+    }
+  }
+</style>
+
+
+
