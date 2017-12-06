52e65e4947b99a3a99b44be4360af6bc8e1263dd
thisang <thisang@foxmail.com>
2017-12-06 23:37:26 +0800

    增加MessageCard组件

---
+++ src/components/messageCard/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="message-card">
+    <p class="message-card-time">{{msgData.time}}</p>
+    <card :header="{title: msgData.headTitle}" :footer="{title: msgData.footTitle}">
+      <div slot="content" class="message-card-content">
+        <p>{{msgData.content}}</p>
+      </div>
+    </card>
+  </div>
+</template>
+<script>
+import { Card } from 'vux'
+export default {
+  name: 'MessageCard',
+  components: {
+    Card
+  },
+  props: {
+    msgData: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .message-card{
+    position: relative;
+    .message-card-time{
+      color: #999;
+      text-align: center;
+      font-size: 12px;
+      margin-top: 10px;
+    }
+    .message-card-content{
+      padding: 10px;
+    }
+  }
+</style>
