5ec4bb7851947c30aad2d6a7fa8ef15659709b4b
thisang <thisang@foxmail.com>
2017-12-06 23:38:33 +0800

    发布页面

--- src/pages/publish/Publish.vue
+++ src/pages/publish/Publish.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="publish">
-    <p>publish</p>
+    <div class="publish-item activiti">
+      <img src="../../../static/images/huodong.png" alt="">
+      <span class="publish-title">活&nbsp;&nbsp;&nbsp;&nbsp;动</span>
+    </div>
+    <div class="publish-item play">
+      <img src="../../../static/images/juwan.png" alt="">
+      <span class="publish-title">聚&nbsp;&nbsp;&nbsp;&nbsp;玩</span>
+    </div>
   </div>
 </template>
 <script>
@@ -15,5 +22,32 @@ export default {
 </script>
 <style lang="scss" scoped>
   .publish{
+    padding: 5px;
+    display: flex;
+    flex-direction: column;
+    .publish-item{
+      margin: 5px;
+      display: flex;
+      flex: 1;
+      background-color: #eb8256;
+      flex-direction: column;
+      align-items: center;
+      img{
+        margin-top: 10px;
+        width: 150px;
+      }
+    }
+    .publish-title{
+      margin-top: 10px;
+      font-size: 20px;
+      text-align: center;
+      color: #fff;
+    }
+    .activiti{
+      background-color: #e44f61;
+    }
+    .play{
+      background-color: #7cb736;
+    }
   }
 </style>
