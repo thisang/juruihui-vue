ec5e350fb8a27dcae79cc6746777971567a7e681
thisang <thisang@foxmail.com>
2017-12-08 00:12:25 +0800

    ActivitiCard组件

---
+++ src/components/activitiCard/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="activiti-card">
+    <div class="img-top">
+      
+    </div>
+    <div class="main-text">
+      <p class="activiti-title">第十七届中国北京动漫嘉年华IDO17</p>
+      <p class="activiti-info">
+        <i class="iconfont icon-position"></i>
+        <span>2016年08月23日 08:00</span>
+      </p>
+      <p class="activiti-info">
+        <i class="iconfont icon-position"></i>        
+        <span>朝阳区</span>
+        <span>天辰东路7号国家会议中心</span>
+        <span>- 1.1km</span>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ActivitiCard'
+}
+</script>
+<style lang="scss" scoped>
+  .activiti-card{
+    background-color: #fff;
+    border-radius: 3px;
+    box-shadow: 0px 1px 3px -1px rgba(0,0,0,.3);
+    .img-top{
+      width: 100%;
+      height: 110px;
+      border-top-left-radius: 3px;
+      border-top-right-radius: 3px;
+      background: url('./images/cbd.jpg') center no-repeat / 100%;
+    }
+    .main-text{
+      padding: 5px;
+      .activiti-title{
+        font-size: 14px;
+        margin-bottom: 3px;
+      }
+      .activiti-info{
+        i{
+          font-size: 12px;
+          color: #333;
+        }
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+</style>
+
--- src/pages/find/Find.vue
+++ src/pages/find/Find.vue
@@ -1,19 +1,64 @@
 <template>
   <div class="find">
-    <p>find</p>
+    <search @result-click="resultClick" :results="results" v-model="searchValue" @on-submit="onSubmit" ref="search"></search>
+    <grid>
+      <grid-item label="Grid">
+        <img class="find-item-icon" slot="icon" src="../../../static/images/today.png">
+        <span class="find-item-label" slot="label">今天</span>
+      </grid-item>
+      <grid-item label="Grid">
+        <img class="find-item-icon" slot="icon" src="../../../static/images/nearby.png">
+        <span class="find-item-label" slot="label">附近</span>
+      </grid-item>
+      <grid-item label="Grid">
+        <img class="find-item-icon" slot="icon" src="../../../static/images/calendar.png">
+        <span class="find-item-label" slot="label">日历</span>
+      </grid-item>
+    </grid>
+    <div class="find-container">
+      <activiti-card></activiti-card>
+    </div>
   </div>
 </template>
 <script>
+import { Search, Grid, GridItem } from 'vux'
+import ActivitiCard from '../../components/activitiCard'
 export default {
   components: {
+    Search,
+    Grid,
+    GridItem,
+    ActivitiCard
+  },
+  data () {
+    return {
+      results: [
+        {
+          title: 'hallo'
+        }
+      ],
+      searchValue: '' // 搜索框文本
+    }
   },
   computed: {
   },
   methods: {
+    resultClick () {
+      console.log('click result')
+    },
+    onSubmit () {
+      console.log('on submit')
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
   .find{
+    .find-item-label{
+      font-size: 12px;
+    }
+    .find-container{
+      padding: 10px;
+    }
   }
 </style>
