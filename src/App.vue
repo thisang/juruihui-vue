3659e6a6602bdaec0e5f905e0725138c88cfd15b
thisang <thisang@foxmail.com>
2017-12-25 23:28:56 +0800

    增加登陆动画

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition :name="transitionName">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      transitionName: null
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      console.log('App.vue => watch => $route.from', from)
+      if (from.name === 'Login') {
+        this.transitionName = 'login'
+      } else {
+        this.transitionName = ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less">
-@import '~vux/src/styles/reset.less';
+  @import '~vux/src/styles/reset.less';
+  body {
+    background-color: #fbf9fe;
+  }
+  .login-enter{
+    opacity: 0;
+    transform: scale(1.5);
+  }
+  .login-enter-active{
+    transition: all .5s ease-out .5s;
+  }
+  .login-enter-to{
+    opacity: 1;
+    transform: scale(1);
+  }
+  .login-leave{
+    opacity: 1;
+  }
+  .login-leave-active{
+    transition: all .5s ease-out;
+  }
+  .login-leave-to{
+    opacity: 0;
+    transform: scale(.5);
+  }
 
-body {
-  background-color: #fbf9fe;
-}
 </style>
