54ef6d05dfad6abdf314f0db1069378e12d9d0e0
thisang <thisang@foxmail.com>
2018-04-02 22:24:56 +0800

    添加代理

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Referer: 'http://localhost:3000'
+        },
+      },
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
